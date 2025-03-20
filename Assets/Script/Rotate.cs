93931b03751c470966f4f81eca9f3ea0ec4e69a7
Kittana575 <kittana.tean@bumail.net>
2025-03-20 09:36:44 +0700

    NewScript

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    [SerializeField] private Vector3 AngularV;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.angularVelocity = AngularV;
     }
 }
