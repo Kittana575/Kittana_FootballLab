d8b0a0c59b6feeb9594a26597b21838072192c6f
Kittana575 <kittana.tean@bumail.net>
2025-03-20 10:11:56 +0700

    updateCode

---
+++ Assets/Script/MagnusEffect.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour
+{
+    private Rigidbody rb;
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            
+        }
+    }
+
+    void kick()
+    {
+        
+    }
+
+    void ApplyMagnusEffect()
+    {
+        
+    }
+}
